ef1b5e2ce9a047d184f4652087bf4c3f777fd31e
Pedro Turcios <Turcios@unlv.nevada.edu>
2019-02-28 10:36:18 -0800

    inital parser

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String args[]){
+        parse_file p_f = new parse_file("C:\\Users\\Pedro\\Downloads\\b_lovely_landscapes.txt");
+    }
+
+}
---
+++ parse_file.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class parse_file {
+    public int size;
+    public parse_file(String file_name){
+        //make file and input
+        File file = new File(file_name);
+        
+        try{
+            Scanner input = new Scanner(file);
+
+            //first line is going to be the size
+            if(input.hasNextInt()) size = input.nextInt();
+
+            //go line by line to address it
+            while(input.hasNextLine()){
+                String current_line = input.nextLine();
+                parse_line(current_line);
+            }
+
+        } catch(Exception e){
+            System.out.println("Something went wrong");
+            System.out.println(e.toString());
+        }
+    }
+
+    //goes by each line and prints it (feeling cute might delete later :3)
+    public void parse_line(String line){
+        System.out.println(line);
+    }
+}
